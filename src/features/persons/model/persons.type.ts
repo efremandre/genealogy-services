65e5d338eba6ff990fd9ba29371dad03ff1d5f5c
Andrei Efremov <samo.dur.andre@gmail.com>
2026-05-03 04:00:29 +0300

    feat(person): add maiden name and middle name

--- src/features/persons/model/persons.type.ts
+++ src/features/persons/model/persons.type.ts
@@ -10,6 +10,10 @@ export type Person = {
 	firstName: string
 	/** Фамилия */
 	lastName: string
+	/** Девичья фамилия */
+	maidenName: string
+	/** Отчество */
+	middleName: string
 	/** Пол (male/female) */
 	gender: 'male' | 'female'
 	/** Год рождения */
@@ -39,6 +43,8 @@ export type PersonsResponse = {
 export type AddRequestPerson = {
 	firstName: string
 	lastName: string
+	maidenName?: string
+	middleName?: string
 	gender: 'male' | 'female'
 	birthYear?: number
 	birthMonth?: number
@@ -52,6 +58,8 @@ export type AddRequestPerson = {
 export type UpdatePerson = {
 	firstName?: string
 	lastName?: string
+	maidenName?: string
+	middleName?: string
 	birthYear?: number
 	birthMonth?: number
 	birthDay?: number
--- src/features/persons/ui/person-mini-card.tsx
+++ src/features/persons/ui/person-mini-card.tsx
@@ -13,8 +13,10 @@ type Props = {
 export const PersonMiniCard = ({ person }: Props) => {
 	const { open } = useModalStore()
 	const { mutate } = useDeletePerson()
-	const { id, firstName, lastName, gender } = person
+	const { id, firstName, lastName, maidenName, middleName, gender } = person
 	const MaleFemaleImage = (gender === 'female') ? FemaleAvatar : MaleAvatar
+
+	const isName = (name: string) => name ? name : ''
 
 	return (
 		<div className='p-2 flex flex-col items-center gap-4 w-[200] rounded-md group relative'>
@@ -26,7 +28,7 @@ export const PersonMiniCard = ({ person }: Props) => {
 					alt='Logo'
 				/>
 			</div>
-			<div className='text-center'>{`${firstName} ${lastName}`}</div>
+			<div className='text-center'>{`${firstName} ${isName(maidenName)} ${lastName} ${isName(middleName)}`}</div>
 			<div className='flex justify-between gap-2 opacity-0 transition group-hover:opacity-100'>
 				<button
 					onClick={() => open(id)}
